2da6a8f12856e43dabf8ab9e27bb963cd018b88b
helloiamnoob <86592051+helliamnoob@users.noreply.github.com>
2023-06-08 13:06:13 +0900

    들여쓰기 공백 수정
    들여쓰기 공백을 2에서 3으로 변경하고 모든 js 포맷 적용하였음

--- src/js/router.js
+++ src/js/router.js
@@ -1,45 +1,39 @@
-import {listing} from '../js/main.js'
-import {hidden} from '../js/about.js'
-
+import { listing } from '../js/main.js';
+import { hidden } from '../js/about.js';
 
 const route = (id, event) => {
-    event = event || window.event;
-    const routeid = id
-    console.log(routeid);
-    if(routeid === 'home')
-    {   
-        
-        event.preventDefault();
-        window.history.pushState({}, "", event.target.href);
-        setTimeout(() => listing(), 100);
-        handleLocation();
-    }
-    else
-    {
-        event.preventDefault();
-        window.history.pushState({}, "", event.target.href);
-        handleLocation();
-        hidden();
-    }
+   event = event || window.event;
+   const routeid = id;
+   console.log(routeid);
+   if (routeid === 'home') {
+      event.preventDefault();
+      window.history.pushState({}, '', event.target.href);
+      setTimeout(() => listing(), 100);
+      handleLocation();
+   } else {
+      event.preventDefault();
+      window.history.pushState({}, '', event.target.href);
+      handleLocation();
+      hidden();
+   }
 };
 
 const routes = {
-    404: "src/pages/404.html",
-    "/index.html" : "src/pages/home.html",
-    "/": "src/pages/home.html",
-    "/detail.html": "src/pages/detail.html",
-    "/about" : "src/pages/about.html",
+   404: 'src/pages/404.html',
+   '/index.html': 'src/pages/home.html',
+   '/': 'src/pages/home.html',
+   '/detail.html': 'src/pages/detail.html',
+   '/about': 'src/pages/about.html',
 };
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
+   const path = window.location.pathname;
+   const route = routes[path] || routes[404];
+   const html = await fetch(route).then((data) => data.text());
+   document.getElementById('main-page').innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
 window.route = route;
 
 //handleLocation();
-
