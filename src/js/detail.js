b2c3abda177ceb0d679af4eb8dbe4600015589fc
Yehye2 <133616786+Yehye2@users.noreply.github.com>
2023-06-09 00:03:39 +0900

    쿠키 임시적용
    쿠키 임시적용

--- src/js/detail.js
+++ src/js/detail.js
@@ -251,164 +251,225 @@ contentInput.addEventListener('input', function () {
   }
 });
 
-// 사용자 로그인 상태 변수
+ // 사용자 로그인 상태 변수
 let isLoggedIn = false;
 
 // 사용자 정보
 let currentUser = null;
 
-// 로그인 상태 확인 함수
+// 사용자 로그인 상태 확인 함수
 function checkLoginStatus() {
+ 
+
   if (isLoggedIn) {
     // 로그인 상태일 때 수행할 동작
-    showLoggedInUI();
+    showLoggedInUI(reviewFormContainer);
   } else {
     // 로그아웃 상태일 때 수행할 동작
-    showLoggedOutUI();
+    showLoggedOutUI(reviewFormContainer);
   }
 }
 
-// 로그인 상태 UI 표시
-function showLoggedInUI() {
-  const logoutBtn = document.getElementById('logoutBtn');
-  const showLoginFormBtn = document.getElementById('showLoginFormBtn');
-  const showSignupFormBtn = document.getElementById('showSignupFormBtn');
-  const reviewFormContainer = document.getElementById('reviewForm');
-  const loginFormContainer = document.getElementById('loginFormContainer');
-  const signupFormContainer = document.getElementById('signupFormContainer');
+ // 쿠키 설정 함수
+ function setCookie(name, value, days) {
+   const date = new Date();
+   date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+   const expires = "expires=" + date.toUTCString();
+   document.cookie = name + "=" + value + ";" + expires + ";path=/";
+ }
 
-  logoutBtn.style.display = 'block';
-  showLoginFormBtn.style.display = 'none';
-  showSignupFormBtn.style.display = 'none';
-  reviewFormContainer.style.display = 'block';
-  loginFormContainer.style.display = 'none';
-  signupFormContainer.style.display = 'none';
-}
+ // 쿠키 가져오기 함수
+ function getCookie(name) {
+   const cookieName = name + "=";
+   const cookies = document.cookie.split(';');
+   for (let i = 0; i < cookies.length; i++) {
+     let cookie = cookies[i];
+     while (cookie.charAt(0) === ' ') {
+       cookie = cookie.substring(1);
+     }
+     if (cookie.indexOf(cookieName) === 0) {
+       return cookie.substring(cookieName.length, cookie.length);
+     }
+   }
+   return "";
+ }
 
-// 로그아웃 상태 UI 표시
-function showLoggedOutUI() {
-  const logoutBtn = document.getElementById('logoutBtn');
-  const showLoginFormBtn = document.getElementById('showLoginFormBtn');
-  const showSignupFormBtn = document.getElementById('showSignupFormBtn');
-  const reviewFormContainer = document.getElementById('reviewForm');
-  const loginFormContainer = document.getElementById('loginFormContainer');
-  const signupFormContainer = document.getElementById('signupFormContainer');
-
-  logoutBtn.style.display = 'none';
-  showLoginFormBtn.style.display = 'block';
-  showSignupFormBtn.style.display = 'block';
-  reviewFormContainer.style.display = 'block';
-  loginFormContainer.style.display = 'none';
-  signupFormContainer.style.display = 'none';
-}
-
-// 로그인 버튼 클릭 시 동작
+const logoutBtn = document.getElementById('logoutBtn');
 const showLoginFormBtn = document.getElementById('showLoginFormBtn');
-const loginFormContainer = document.getElementById('loginFormContainer');
-
-showLoginFormBtn.addEventListener('click', function () {
-  if (loginFormContainer.style.display === 'block') {
-    loginFormContainer.style.display = 'none';
-  } else {
-    loginFormContainer.style.display = 'block';
-    signupFormContainer.style.display = 'none';
-  }
-});
-
-// 회원가입 버튼 클릭 시 동작
 const showSignupFormBtn = document.getElementById('showSignupFormBtn');
+const loginFormContainer = document.getElementById('loginFormContainer');
 const signupFormContainer = document.getElementById('signupFormContainer');
+ const reviewFormContainer = document.getElementById('reviewForm');
 
-showSignupFormBtn.addEventListener('click', function () {
-  if (signupFormContainer.style.display === 'block') {
-    signupFormContainer.style.display = 'none';
-  } else {
-    signupFormContainer.style.display = 'block';
-    loginFormContainer.style.display = 'none';
-  }
-});
-// 사용자 등록
-document.getElementById("signupForm").addEventListener("submit", function(event) {
+ // 로그인 상태 UI 표시
+ function showLoggedInUI() {
+   logoutBtn.style.display = 'block';
+   showLoginFormBtn.style.display = 'none';
+   showSignupFormBtn.style.display = 'none';
+   reviewFormContainer.style.display = 'block';
+   loginFormContainer.style.display = 'none';
+   signupFormContainer.style.display = 'none';
+ }
+
+ // 로그아웃 상태 UI 표시
+ function showLoggedOutUI() {
+   logoutBtn.style.display = 'none';
+   showLoginFormBtn.style.display = 'block';
+   showSignupFormBtn.style.display = 'block';
+   reviewFormContainer.style.display = 'block';
+   loginFormContainer.style.display = 'none';
+   signupFormContainer.style.display = 'none';
+ }
+
+ // 로그인 버튼 클릭 시 동작
+ showLoginFormBtn.addEventListener('click', function () {
+   if (loginFormContainer.style.display === 'block') {
+     loginFormContainer.style.display = 'none';
+   } else {
+     loginFormContainer.style.display = 'block';
+     signupFormContainer.style.display = 'none';
+   }
+ });
+
+ // 회원가입 버튼 클릭 시 동작
+ showSignupFormBtn.addEventListener('click', function () {
+   if (signupFormContainer.style.display === 'block') {
+     signupFormContainer.style.display = 'none';
+   } else {
+     signupFormContainer.style.display = 'block';
+     loginFormContainer.style.display = 'none';
+   }
+ });
+
+ // 사용자 등록
+ document.getElementById("signupForm").addEventListener("submit", function (event) {
+   event.preventDefault();
+
+   // 사용자명과 비밀번호 값을 가져옴
+   var username = document.getElementById("registerUsername").value;
+   var password = document.getElementById("registerPassword").value;
+
+   registerUser(username, password);
+ });
+
+ function registerUser(username, password) {
+   let users = localStorage.getItem('users');
+   if (users) {
+     users = JSON.parse(users);
+   } else {
+     users = [];
+   }
+
+   // 사용자명이 이미 존재하는지 확인
+   const existingUser = users.find(user => user.username === username);
+   if (existingUser) {
+     alert('이미 존재하는 사용자명입니다. 다른 사용자명을 선택해주세요.');
+     return;
+   }
+
+   const user = {
+     username: username,
+     password: password
+   };
+
+   users.push(user);
+   localStorage.setItem('users', JSON.stringify(users));
+   alert('사용자 등록이 완료되었습니다.');
+   document.getElementById("signupForm").reset();
+ }
+
+ // 로그인
+ document.getElementById("loginForm").addEventListener("submit", function (event) {
+   event.preventDefault();
+
+   // 사용자명과 비밀번호 값을 가져옴
+   var username = document.getElementById("loginUsername").value;
+   var password = document.getElementById("loginPassword").value;
+
+   loginUser(username, password);
+ });
+
+ document.getElementById("commentForm").addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // 사용자명과 비밀번호 값을 가져옴
-  var username = document.getElementById("registerUsername").value;
-  var password = document.getElementById("registerPassword").value;
+  if (isLoggedIn) {
+    // 로그인 상태에서는 비밀번호 칸을 제외하고 사용자명과 댓글 내용만 가져옴
+    let username = currentUser.username;
+    let comment = document.getElementById("commentContent").value;
 
-  registerUser(username, password);
-});
-
-function registerUser(username, password) {
-  let users = localStorage.getItem('users');
-  if (users) {
-    users = JSON.parse(users);
-  } else {
-    users = [];
-  }
-
-  // 사용자명이 이미 존재하는지 확인
-  const existingUser = users.find(user => user.username === username);
-  if (existingUser) {
-    alert('이미 존재하는 사용자명입니다. 다른 사용자명을 선택해주세요.');
-    return;
-  }
-
-  const user = {
-    username: username,
-    password: password
-  };
-
-  users.push(user);
-  localStorage.setItem('users', JSON.stringify(users));
-  alert('사용자 등록이 완료되었습니다.');
-  document.getElementById("signupForm").reset();
-}
-
-// 로그인
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  // 사용자명과 비밀번호 값을 가져옴
-  var username = document.getElementById("loginUsername").value;
-  var password = document.getElementById("loginPassword").value;
-
-  loginUser(username, password);
-});
-
-function loginUser(username, password) {
-  let users = localStorage.getItem('users');
-  if (users) {
-    users = JSON.parse(users);
-    const user = users.find(user => user.username === username && user.password === password);
-    if (user) {
-      alert('로그인에 성공했습니다.');
-      isLoggedIn = true; // 로그인 상태 변수 변경
-      currentUser = user; // 현재 사용자 설정
-      checkLoginStatus(); // 로그인 상태 확인 및 UI 업데이트
-      
-      // 로그인 폼 숨기기
-      loginFormContainer.style.display = 'none';
+    if (comment.trim() !== "") {
+      // 댓글을 추가하는 로직
+      addComment(username, comment);
+      document.getElementById("commentContent").value = ""; // 댓글 입력란 초기화
     } else {
-      alert('사용자명 또는 비밀번호가 올바르지 않습니다.');
+      alert("댓글을 입력해주세요.");
     }
   } else {
-    alert('등록된 사용자가 없습니다. 먼저 사용자를 등록해주세요.');
+    alert("먼저 로그인해주세요.");
+  }
+});
+
+ function loginUser(username) {
+   let users = localStorage.getItem('users');
+   if (users) {
+     users = JSON.parse(users);
+     const user = users.find(user => user.username === username);
+     if (user) {
+       alert('로그인에 성공했습니다.');
+       isLoggedIn = true; // 로그인 상태 변수 변경
+      currentUser = { username: username }; // 현재 사용자 설정
+      checkLoginStatus(); // 로그인 상태 확인 및 UI 업데이트
+
+       // 쿠키에 로그인 상태 저장
+       setCookie('isLoggedIn', 'true', 7);
+
+     } else {
+       alert('사용자명 또는 비밀번호가 올바르지 않습니다.');
+     }
+   } else {
+     alert('등록된 사용자가 없습니다. 먼저 사용자를 등록해주세요.');
+   }
+ }
+
+ function checkLoginStatus() {
+  if (isLoggedIn) {
+    // 로그인 상태일 경우
+    document.getElementById("loginForm").style.display = "none"; // 로그인 폼 숨김
+    document.getElementById("commentForm").style.display = "block"; // 댓글 작성 폼 표시
+  } else {
+    // 비로그인 상태일 경우
+    document.getElementById("loginForm").style.display = "block"; // 로그인 폼 표시
+    document.getElementById("commentForm").style.display = "none"; // 댓글 작성 폼 숨김
   }
 }
 
-// 로그아웃 버튼 클릭 이벤트 처리
-const logoutBtn = document.getElementById('logoutBtn');
+ // 로그아웃 버튼 클릭 이벤트 처리
+ logoutBtn.addEventListener('click', function () {
+   logout();
+ });
 
-logoutBtn.addEventListener('click', function () {
-  logout();
-});
+ // 로그아웃 동작 함수
+ function logout() {
+   isLoggedIn = false; // 로그인 상태 변수 변경
+   currentUser = null; // 현재 사용자 초기화
+   checkLoginStatus(); // 로그인 상태 확인 및 UI 업데이트
 
-// 로그아웃 동작 함수
-function logout() {
-  isLoggedIn = false; // 로그인 상태 변수 변경
-  currentUser = null; // 현재 사용자 초기화
-  checkLoginStatus(); // 로그인 상태 확인 및 UI 업데이트
-}
+   // 쿠키에서 로그인 상태 제거
+   setCookie('isLoggedIn', 'false', 0);
+ }
 
-// 초기 로그인 상태 확인 및 UI 설정
-checkLoginStatus();
+ // 초기 로그인 상태 확인 및 UI 설정
+ checkLoginStatus();
+ 
+ // 페이지 로드 시 쿠키에서 로그인 상태 확인
+ window.addEventListener('load', function () {
+   const isLoggedInCookie = getCookie('isLoggedIn');
+   if (isLoggedInCookie === 'true') {
+     isLoggedIn = true;
+   } else {
+     isLoggedIn = false;
+   }
+   checkLoginStatus();
+ });
+
+ 
